a550d099cf3bbce160ace73539bc2e5b84571c62
vamsi73 <vamsicse73@gmail.com>
2018-11-18 10:25:17 +0530

    added git-demo projec to loal repo

---
+++ src/main/java/com/test/git_demo/Test.java
@@ -0,0 +1,15 @@
+package com.test.git_demo;
+
+public class Test {
+	
+	public void m1() {
+		System.out.println("git app");
+	}
+	
+	
+public static void main(String... ar) {
+	
+	System.out.println("for testing Git");
+	
+}
+}
